d288fc218fc2ea74cd6c0c7d6dc4f8172adb2605
federalolive <j3pumphrey@gmail.com>
2021-02-02 11:59:22 -0600

    Resolve pathing issue for home page.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Band
 
+def BandCreate(CreateView):
+    model = Band
+    fields = '__all__'
 
 def home(request):
     return render(request, 'home.html')
