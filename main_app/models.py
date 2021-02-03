13eeed1189f18a77509673d53d354f9a6212c40e
federalolive <j3pumphrey@gmail.com>
2021-02-02 19:51:57 -0600

    Update Cat model, import reverse.

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Band(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
@@ -8,3 +8,7 @@ class Band(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"band_id": self.id})
+    
