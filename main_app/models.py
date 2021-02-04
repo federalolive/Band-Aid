10ffe0a4d911e4dff47ccebf1f10eff8212a5d39
federalolive <j3pumphrey@gmail.com>
2021-02-04 08:37:56 -0600

    Add user to models.

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 class Band(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     dateformed = models.CharField(max_length=4)
     description = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
