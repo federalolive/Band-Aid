c82ec4e02807682717e64338b0ae6c9e80a9b9ed
federalolive <j3pumphrey@gmail.com>
2021-02-04 10:31:27 -0600

    Add to similarband model.

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 class SimilarBand(models.Model):
     name = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
