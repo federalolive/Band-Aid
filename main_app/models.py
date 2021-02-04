08b97884ff1909d653877840c0e9bc8c78a9ddaf
federalolive <j3pumphrey@gmail.com>
2021-02-04 10:25:57 -0600

    Stub up SimilarBands model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+class SimilarBand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('bands_detail', kwargs={'pk': self.id})
 class Band(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     dateformed = models.CharField(max_length=4)
     description = models.TextField(max_length=250)
+    SimilarBands = models.ManyToManyField(SimilarBand)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -21,4 +30,3 @@ class Album(models.Model):
 
 def __str__(self):
     return f"{self.get_album_display()} on {self.date}"
-
