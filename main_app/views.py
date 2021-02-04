9bbb5127a44e984c7f6049ecf0b97b99d0b11d04
federalolive <j3pumphrey@gmail.com>
2021-02-04 10:01:28 -0600

    Fix issues that result in some functional issues, stub up code in base/detail.html files.

--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,10 @@ class BandCreate(LoginRequiredMixin, CreateView):
     fields = ['name', 'genre', 'dateformed', 'description']
     success_url = '/bands/'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 class BandUpdate(LoginRequiredMixin, UpdateView):
     model = Band
     fields = ['genre', 'description']
